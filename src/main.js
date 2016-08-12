2850415cb5e22eda751d008e6fa5425142a48aa1
James Cox-Morton <th3james@fastmail.fm>
2016-08-12 18:48:27 +0100

    Add colour buffers and shader params

--- src/main.js
+++ src/main.js
@@ -78,6 +78,14 @@
     return vertexPositionAttribute;
   }
 
+  var initVertexColorAttribute = function(gl, program) {
+    var vertexColorAttribute = gl.getAttribLocation(
+      program, "aVertexColor"
+    );
+    gl.enableVertexAttribArray(vertexColorAttribute);
+    return vertexColorAttribute;
+  }
+
   var initBuffers = function(gl) {
     var squareVerticesBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
@@ -94,6 +102,20 @@
     );
     return squareVerticesBuffer;
   };
+
+  var initColorBuffer = function(gl) {
+    var colors = [
+      1.0,  1.0,  1.0,  1.0,    // white
+      1.0,  0.0,  0.0,  1.0,    // red
+      0.0,  1.0,  0.0,  1.0,    // green
+      0.0,  0.0,  1.0,  1.0     // blue
+    ];
+
+    var squareVerticesColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    return squareVerticesColorBuffer;
+  }
 
   var loadIdentity = function() {
     mvMatrix = Matrix.I(4);
@@ -116,7 +138,7 @@
   }
 
   var horizAspect = 480.0/640.0;
-  var drawScene = function(gl, program, squareVerticesBuffer, vertexPositionAttribute) {
+  var drawScene = function(gl, program, squareVerticesBuffer, squareVerticesColorBuffer, vertexPositionAttribute, vertexColorAttribute) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     var perspectiveMatrix = makePerspective(45, horizAspect, 0.1, 100.0);
 
@@ -127,6 +149,10 @@
     gl.vertexAttribPointer(
       vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0
     );
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+    gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+
     setMatrixUniforms(perspectiveMatrix, program);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   }
@@ -152,13 +178,17 @@
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.viewport(0, 0, canvas.width, canvas.height);
 
-    var shaderProgram = initShaders(gl);
     var squareVerticesBuffer = initBuffers(gl)
+    var colorBuffer = initColorBuffer(gl);
+    var shaderProgram = initShaders(gl);
     var vertexPositionAttribute = initVertexPositionAttribute(
       gl, shaderProgram
     );
+    var vertexColorAttribute = initVertexColorAttribute(
+      gl, shaderProgram
+    );
     drawScene(
-      gl, shaderProgram, squareVerticesBuffer, vertexPositionAttribute
+      gl, shaderProgram, squareVerticesBuffer, colorBuffer, vertexPositionAttribute, vertexColorAttribute
     );
   };
 

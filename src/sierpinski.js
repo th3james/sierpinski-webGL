418c2ba044b048bf40ec4944ed06b70c5d277002
James Cox-Morton <th3james@fastmail.fm>
2016-08-14 18:00:37 +0100

    Keep triangles unflattened for longer

--- src/sierpinski.js
+++ src/sierpinski.js
@@ -14,15 +14,11 @@
     }
   };
 
-  var partitionThirds = function (arr) {
-    var thirdLength = arr.length/3;
-
-    return [0,1,2].map(function(i) {
-      var start = i*thirdLength;
-      var end = (i+1)*thirdLength;
-      return arr.slice(start, end);
-    });
-  };
+  var flattenTriangles = function (triangles) {
+    return triangles.reduce(function(flattenedTriangle, triangle) {
+      return flattenedTriangle.concat(triangle);
+    }, [])
+  }
 
   window.Sierpinski.generateVertices = function(startTriangle, levels) {
     var newTriangles = [];
@@ -30,23 +26,22 @@
     // for each vertex
     eachPair(startTriangle, function(rootVertex) {
       // for each other vertex
+      var newTriangle = []
       eachPair(startTriangle, function(otherVertex) {
         var mid = Sierpinski.midPoint(
           rootVertex[0], rootVertex[1], otherVertex[0], otherVertex[1]
         );
-        newTriangles.push(mid[0]);
-        newTriangles.push(mid[1]);
+        newTriangle.push(mid[0]);
+        newTriangle.push(mid[1]);
       });
+      newTriangles.push(newTriangle);
     });
 
-    if (levels <= 1) {
-      return newTriangles;
-    } else {
-      return partitionThirds(newTriangles).map(function (triangle) {
+    if (levels > 1) {
+      newTriangles = newTriangles.map(function (triangle) {
         return Sierpinski.generateVertices(triangle, levels - 1);
-      }).reduce(function(allSubTriangles, subTriangles) {
-        return allSubTriangles.concat(subTriangles);
-      }, []);
+      })
     }
+    return flattenTriangles(newTriangles);
   };
 })();

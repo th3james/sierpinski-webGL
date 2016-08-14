1abc82432f5b1d9e72d34cc6ff4d8a1e362a9f3f
James Cox-Morton <th3james@fastmail.fm>
2016-08-14 16:44:22 +0100

    Fix perspective

--- src/main.js
+++ src/main.js
@@ -28,16 +28,15 @@
   };
 
   var drawScene = function (
-    gl, program, horizAspect, verticesBuffer, vertexPositionAttribute,
+    gl, program, perspective, verticesBuffer, vertexPositionAttribute,
     cameraPosition
   ) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var perspectiveMatrix = makePerspective(45, horizAspect, 0.1, 100.0);
     var mvMatrix = WebGLHelpers.calculateMvMatrix(
       WebGLHelpers.identity(), cameraPosition
     );
-    WebGLHelpers.setMatrixUniforms(gl, perspectiveMatrix, mvMatrix, program);
+    WebGLHelpers.setMatrixUniforms(gl, perspective, mvMatrix, program);
     
     gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
     gl.vertexAttribPointer(
@@ -50,33 +49,33 @@
 
   var TICK_WAIT = 16; // roughly 60FPS
   var renderLoop = function (
-    gl, triangles, horizAspect, verticesBuffer, shaderProgram,
+    gl, triangles, perspective, verticesBuffer, shaderProgram,
     vertexPositionAttribute, cameraPosition
   ) {
     drawScene(
-      gl, shaderProgram, horizAspect, verticesBuffer, vertexPositionAttribute, cameraPosition
+      gl, shaderProgram, perspective, verticesBuffer, vertexPositionAttribute,
+      cameraPosition
     );
 
-    setTimeout(function() {
+    setTimeout(function () {
       renderLoop(
-        gl, triangles, horizAspect, verticesBuffer, shaderProgram,
+        gl, triangles, perspective, verticesBuffer, shaderProgram,
         vertexPositionAttribute, cameraPosition
       );
     }, TICK_WAIT); 
   };
 
   var startRenderLoop = function(
-    gl, canvasEvents, sliderEvents, initialTriangles
+    gl, shaderProgram, width, height, canvasEvents, sliderEvents,
+    initialTriangles
   ) {
     var verticesBuffer = initBuffers(gl, initialTriangles);
-    var shaderProgram = WebGLHelpers.initShaders(
-      gl, "shader-fs", "shader-vs"
-    );
     var vertexPositionAttribute = initVertexPositionAttribute(
       gl, shaderProgram
     );
 
-    var horizAspect = 480.0/640.0;
+    var perspective = makePerspective(45, height/width, 0.1, 100.0);
+
     var MIN_ZOOM = -3.25;
     var MAX_ZOOM = -0.2;
     var cameraPosition = [0.0, 0.0, MIN_ZOOM];
@@ -91,7 +90,7 @@
     });
 
     renderLoop(
-      gl, initialTriangles, horizAspect, verticesBuffer,
+      gl, initialTriangles, perspective, verticesBuffer,
       shaderProgram, vertexPositionAttribute, cameraPosition
     );
   };
@@ -106,12 +105,19 @@
     var $slider = $("#zoom-slider");
     var sliderEvents = SliderEventStream($slider);
 
+    var shaderProgram = WebGLHelpers.initShaders(
+      gl, "shader-fs", "shader-vs"
+    );
+
     var startTriangle = [
       0.0, 1.0,
       -1.0, -1.0,
       1.0, -1.0,
     ];
-    startRenderLoop(gl, canvasEvents, sliderEvents, startTriangle);
+    startRenderLoop(
+      gl, shaderProgram, canvas.width, canvas.height,
+      canvasEvents, sliderEvents, startTriangle
+    );
   };
 
 }());

ebead0dd437503acae53b04a084a627861ff0d7e
James Cox-Morton <th3james@fastmail.fm>
2016-08-14 18:19:12 +0100

    Semi-colons

--- src/sierpinski.js
+++ src/sierpinski.js
@@ -9,7 +9,7 @@
   };
 
   var mapPairs = function (arr, fn) {
-    var result = []
+    var result = [];
     for(var i = 0; i < arr.length; i += 2) {
       result.push(
         fn(arr.slice(i, i+2), i)
@@ -21,15 +21,15 @@
   var flatten = function (arr) {
     return arr.reduce(function(flattened, subArr) {
       return flattened.concat(subArr);
-    }, [])
-  }
+    }, []);
+  };
 
   window.Sierpinski.generateVertices = function(startTriangle, levels) {
     var newTriangles = mapPairs(startTriangle, function(rootVertex) {
       // collect mid points between each vertex 
       var midPoints = mapPairs(startTriangle, function(otherVertex) {
         return Sierpinski.midPoint(rootVertex, otherVertex);
-      })
+      });
 
       return flatten(midPoints);
     });
@@ -37,7 +37,7 @@
     if (levels > 1) {
       newTriangles = newTriangles.map(function (triangle) {
         return Sierpinski.generateVertices(triangle, levels - 1);
-      })
+      });
     }
     return flatten(newTriangles);
   };

ee4fdfdb9a7e3b02769eebecb1ce04fa350b7db0
James Cox-Morton <th3james@fastmail.fm>
2016-08-18 08:38:07 +0100

    Add specs for more conditions for triangleOverlapsFrustum

--- specs/webgl_helpers_spec.js
+++ specs/webgl_helpers_spec.js
@@ -73,5 +73,47 @@ describe("WebGLHelpers", function () {
         ).toEqual(false);
       });
     });
+
+    describe("given a triangle intersects but mostly right of the frustum", function () {
+      it("returns true", function () {
+        // view pushed left till only left most triangle point intersects
+        var modelViewAtLoad = $M([
+          [3.2189514164974593, 0, 0, 3.958304319974223],
+          [0, 2.4142135623730945, 0, -0.18609562876625918],
+          [0, 0, -1.0002000200020003, 3.230648064806481],
+          [0, 0, -1, 3.25]
+        ]);
+        var triangle = [
+          0.0, 1.0,
+          -1.0, -1.0,
+          1.0, -1.0,
+        ];
+
+        expect(
+          WebGLHelpers.triangleInFrustum(modelViewAtLoad, triangle)
+        ).toEqual(true);
+      });
+    });
+
+    describe("given a triangle that encompasses the frustum", function () {
+      it("returns true", function () {
+        // view zoomed till all points of triangle are outside
+        var modelViewAtLoad = $M([
+          [3.2189514164974593, 0, 0, 0.0201184463531126],
+          [0, 2.4142135623730945, 0, 0.04526650429449618],
+          [0, 0, -1.0002000200020003, 1.4214862686268626],
+          [0, 0, -1, 1.4411999999999998]
+        ]);
+        var triangle = [
+          0.0, 1.0,
+          -1.0, -1.0,
+          1.0, -1.0,
+        ];
+
+        expect(
+          WebGLHelpers.triangleInFrustum(modelViewAtLoad, triangle)
+        ).toEqual(true);
+      });
+    });
   });
 });
